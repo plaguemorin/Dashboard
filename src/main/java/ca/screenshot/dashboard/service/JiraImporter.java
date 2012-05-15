205785899431cee593bd1630942aef82dda48497
Philippe Lague-Morin <philippe@screenshot.ca>
2012-05-15 18:34:37 -0400

    Dashboard v2

--- src/main/java/ca/screenshot/dashboard/service/JiraImporter.java
+++ src/main/java/ca/screenshot/dashboard/service/JiraImporter.java
@@ -2,9 +2,11 @@ package ca.screenshot.dashboard.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.net.URL;
 
 /**
  * @author plaguemorin
@@ -15,9 +17,11 @@ import javax.annotation.PostConstruct;
 public class JiraImporter {
 	private static final Logger LOGGER = LoggerFactory.getLogger(JiraImporter.class);
 
+	@Value("${jiraImporter.jiraUrl}")
+	private URL jiraUrl;
 
 	@PostConstruct
 	public void init() {
-
+		LOGGER.info("Using JIRA url: " + jiraUrl.toExternalForm());
 	}
 }
